6d866977599c0ad06267226dc97fabbf2c715311
Mariia <m.goncharenkoo@icloud.com>
2023-01-24 00:09:29 -0500

    passed tests

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,5 +1,23 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+
+  // if an array is empty, return empty array
+  if (arr.length === 0) {
+    return [];
+  }
+
+  // store value of the smallest number in variable 'min'
+  const min = Math.min(...arr);
+  // find index of the smallest number
+  const idx = arr.indexOf(min);
+  // delete the smallest element from the array
+  arr.splice(idx, 1);
+
+  // store calling a recursive function in a variable
+  const result = selectionSortRecursive(arr);
+  // call recursive variable and prepend the smallest number
+  result.unshift(min);
+  // return sorted array
+  return result;
 }
 
 if (require.main === module) {
